8ddd57af415a300c0212c7173e3fcf08ea5d5c90
initdc <initd@outlook.com>
2023-01-10 17:13:51 +0000

    build: add base by `bundle gem make-env`

---
+++ lib/make/env.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "env/version"
+
+module Make
+  module Env
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/make/env/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Make
+  module Env
+    VERSION = "0.1.0"
+  end
+end
